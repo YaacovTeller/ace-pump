272719e1f29a4263a4e0b6bef2543b05108a38a0
Yaacov Teller <yaacovteller@gmail.com>
2024-02-17 21:21:30 +0200

    customerAccessList

--- AcePump.Web/Models/AcePumpProfileModel.vb
+++ AcePump.Web/Models/AcePumpProfileModel.vb
@@ -5,8 +5,12 @@ Namespace Models
     Public Class AcePumpProfileModel
         Public Property UserID As Integer
 
-        <UIHint("_CustomerID")> _
-        <DisplayName("Customer")> _
+        <UIHint("_CustomerID")>
+        <DisplayName("Customer")>
         Public Property CustomerID As Integer?
+
+        <UIHint("_CustomerAccess")>
+        <DisplayName("Customer Access List")>
+        Public Property CustomerAccessList As Integer?
     End Class
 End Namespace
